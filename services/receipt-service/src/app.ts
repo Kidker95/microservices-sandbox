74053062c59cde135fb4baeba404648096ed6fe7
Omri <irmoomri@gmail.com>
2025-12-09 15:40:54 +0200

    initial receipt-service app is running

---
+++ services/receipt-service/src/app.ts
@@ -0,0 +1,43 @@
+import express, { Express, Request, Response } from "express";
+import { errorMiddleware } from "./middleware/error-middleware";
+import { env } from "./config/env";
+
+
+export class App {
+    public readonly server: Express;
+
+    constructor() {
+        this.server = express();
+        this.registerInfra();
+        this.registerRoutes();
+        this.registerErrorHandling();
+
+    }
+
+    private registerInfra(): void {
+        this.server.use(express.json());
+    }
+
+    private registerRoutes(): void {
+        this.server.get("/health", (req: Request, res: Response) => {
+            res.json({ status: "ok", service: "receipt-service" });
+        });
+        // this.server.use("/api/products", receiptRouter);
+    }
+
+    private registerErrorHandling(): void {
+        this.server.use(errorMiddleware.routeNotFound);
+        this.server.use(errorMiddleware.catchAll);
+    }
+
+    public start() {
+        this.server.listen(env.port, () => {
+            console.log(`Receipt service running on port ${env.port}`);
+        })
+    }
+}
+
+export const app = new App();
+
+const bootstrap = async (): Promise<void> => app.start();
+bootstrap();
